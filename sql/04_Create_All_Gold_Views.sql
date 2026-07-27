652b10943ade3015b40511c031de3f4084a5116b
Nikhil Manne <133259754+nikhilmanne10@users.noreply.github.com>
2026-07-27 17:35:29 +0530

    Create 04_Create_All_Gold_Views.sql

---
+++ sql/04_Create_All_Gold_Views.sql
@@ -0,0 +1,71 @@
+EXEC CreateSQLServerlessView_gold 'BillOfMaterials';
+GO
+
+EXEC CreateSQLServerlessView_gold 'Culture';
+GO
+
+EXEC CreateSQLServerlessView_gold 'Illustration';
+GO
+
+EXEC CreateSQLServerlessView_gold 'Location';
+GO
+
+EXEC CreateSQLServerlessView_gold 'Product';
+GO
+
+EXEC CreateSQLServerlessView_gold 'ProductCategory';
+GO
+
+EXEC CreateSQLServerlessView_gold 'ProductCostHistory';
+GO
+
+EXEC CreateSQLServerlessView_gold 'ProductDescription';
+GO
+
+EXEC CreateSQLServerlessView_gold 'ProductDocument';
+GO
+
+EXEC CreateSQLServerlessView_gold 'ProductInventory';
+GO
+
+EXEC CreateSQLServerlessView_gold 'ProductListPriceHistory';
+GO
+
+EXEC CreateSQLServerlessView_gold 'ProductModel';
+GO
+
+EXEC CreateSQLServerlessView_gold 'ProductModelIllustration';
+GO
+
+EXEC CreateSQLServerlessView_gold 'ProductModelProductDescriptionCulture';
+GO
+
+EXEC CreateSQLServerlessView_gold 'ProductPhoto';
+GO
+
+EXEC CreateSQLServerlessView_gold 'ProductProductPhoto';
+GO
+
+EXEC CreateSQLServerlessView_gold 'ProductReview';
+GO
+
+EXEC CreateSQLServerlessView_gold 'ProductSubcategory';
+GO
+
+EXEC CreateSQLServerlessView_gold 'ScrapReason';
+GO
+
+EXEC CreateSQLServerlessView_gold 'TransactionHistory';
+GO
+
+EXEC CreateSQLServerlessView_gold 'TransactionHistoryArchive';
+GO
+
+EXEC CreateSQLServerlessView_gold 'UnitMeasure';
+GO
+
+EXEC CreateSQLServerlessView_gold 'WorkOrder';
+GO
+
+EXEC CreateSQLServerlessView_gold 'WorkOrderRouting';
+GO
